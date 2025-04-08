676a9f4605c3f5ad887e721abcca4caeb5705fd7
JuniorFs <juniorfernandessilva1@gmail.com>
2025-04-08 19:54:29 -0300

    Using the components created and adjusting the sidebar
    1 - importing all the Icons
    2 - defining the useEffects to handle the active buttons (lists: Priorities / Projects)
    3 - adjusting some css (text-color and bg-color)
    4 - Setting up the Navbar Links / Projects (button) and Priorities (button) with some pre-established filters

--- client/src/app/components/Sidebar/index.tsx
+++ client/src/app/components/Sidebar/index.tsx
@@ -1,29 +1,67 @@
 'use client';
-import { LockIcon } from 'lucide-react';
+import { useAppDispatch, useAppSelector } from '@/app/redux';
+import { setIsSideBarCollapsed } from '@/state';
+import {
+	AlertCircle,
+	AlertOctagon,
+	AlertTriangle,
+	Briefcase,
+	ChevronDown,
+	ChevronUp,
+	House,
+	Layers3,
+	LockIcon,
+	LucideIcon,
+	Search,
+	Settings,
+	ShieldAlert,
+	User,
+	Users,
+	X,
+} from 'lucide-react';
 import Image from 'next/image';
+import { usePathname } from 'next/navigation';
 import { useState } from 'react';
+import Link from 'next/link';
 
 const Sidebar = () => {
-	const [ShowCondominios, SetShowCondominios] = useState<boolean>(true);
-	const [ShowPriority, SetShowPriority] = useState<boolean>(true);
+	const [ShowProjects, SetShowProjects] = useState<boolean>(true);
+	const [ShowPriorities, SetShowPriorities] = useState<boolean>(true);
 
-	const sidebarClassNames =
-		' fixed flex flex-col h-[100%] justify-between shadow-xl transition-all duration-300 h-full z-60 dark:bg-black overflow-y-auto bg-white w-64';
+	const dispatch = useAppDispatch();
+	const isSideBarCollapsed = useAppSelector(
+		(state) => state.global.isSideBarCollapsed
+	);
+
+	const sidebarClassNames = `fixed flex flex-col h-[100%] justify-between shadow-xl transition-all duration-300 h-full z-60 
+		dark:bg-black overflow-y-auto bg-white ${
+			isSideBarCollapsed ? 'w-0 hidden' : 'w-64'
+		} `;
 
 	return (
 		<div className={sidebarClassNames}>
 			<div className="flex h-[100%] w-full flex-col justify-start">
 				{/* TOP LOGO */}
 				<div className="z-50 flex min-h-[45px] w-64 items-center justify-between bg-white px-[1.5rem] pt-3 dark:bg-black">
-					<div className="text-xl font-bold text-gray-900 dark:text-white">
+					<div className="text-xl font-bold text-gray-700 dark:text-white">
 						Orion
 					</div>
+					{isSideBarCollapsed ? null : (
+						<button
+							className="py-3"
+							onClick={() => {
+								dispatch(setIsSideBarCollapsed(!isSideBarCollapsed));
+							}}
+						>
+							<X className="h-6 w-6 text-gray-800 hover:text-gray-500 dark:text-white" />
+						</button>
+					)}
 				</div>
 				{/* TEAM */}
 				<div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-[1.5rem] py-4 dark:border-gray-700">
-					<Image src="/logo-3.jpg" alt="Logo" width={45} height={45} />
+					<Image src="/logo-3.jpg" alt="Logo" width={45} height={45} priority />
 					<div className="flex-row items-center">
-						<h3 className="text-md font-bold dark:text-gray-200 dark:text-white tracking-wide">
+						<h3 className="text-md font-bold dark:text-gray-200 tracking-wide">
 							Menu
 						</h3>
 						<div className="mt-1 flex items-start gap-2">
@@ -33,6 +71,68 @@ const Sidebar = () => {
 					</div>
 				</div>
 				{/* NAV BAR LINKS */}
+				<nav className="z-10 w-full">
+					<SidebarLink icon={House} label="House" href="/" />
+					<SidebarLink icon={Briefcase} label="Time Line" href="/TimeLine" />
+					<SidebarLink icon={Search} label="Search" href="/Search" />
+					<SidebarLink icon={Settings} label="Setting" href="/Setting" />
+					<SidebarLink icon={User} label="User" href="/User" />
+					<SidebarLink icon={Users} label="Team" href="/Team" />
+				</nav>
+
+				{/** PROJECTS LINKS*/}
+				<button
+					onClick={() => SetShowProjects((prev): boolean => !prev)}
+					className="flex w-full items-center justify-between px-8 py-3 text-gray-700 dark:text-gray-300"
+				>
+					<span className="">Projects</span>
+					{ShowProjects ? (
+						<ChevronUp className="h-5 w-5" />
+					) : (
+						<ChevronDown className="h-5 w-5" />
+					)}
+				</button>
+
+				{/** PROJECTS LIST */}
+
+				{/** PRIORITIES LIST*/}
+				<button
+					onClick={() => SetShowPriorities((prev): boolean => !prev)}
+					className="flex w-full items-center justify-between px-8 py-3 text-gray-700 dark:text-gray-300"
+				>
+					<span className="">Priorities</span>
+					{ShowPriorities ? (
+						<ChevronUp className="h-5 w-5" />
+					) : (
+						<ChevronDown className="h-5 w-5" />
+					)}
+				</button>
+
+				{ShowPriorities && (
+					<nav className="z-10 w-full">
+						<SidebarLink
+							icon={AlertCircle}
+							label="Urgent"
+							href="/priority/urgent"
+						/>
+						<SidebarLink
+							icon={ShieldAlert}
+							label="High"
+							href="/priority/high"
+						/>
+						<SidebarLink
+							icon={AlertTriangle}
+							label="Medium"
+							href="/priority/medium"
+						/>
+						<SidebarLink icon={AlertOctagon} label="Low" href="/priority/low" />
+						<SidebarLink
+							icon={Layers3}
+							label="Backlog"
+							href="/priority/backlog"
+						/>
+					</nav>
+				)}
 			</div>
 		</div>
 	);
