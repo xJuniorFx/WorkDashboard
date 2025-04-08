7831bef96e7839b31ecf31bb9cc9e433d09ab88d
JuniorFs <juniorfernandessilva1@gmail.com>
2025-04-08 19:49:37 -0300

    Crating a sidebar component (nav links)
    Creating the navbar links
    
    1 - defining the interface and its types
    2 - using the pathName to keep track of the href tha is active, so we can adjust the css acordiling
    3 - creating the return (div / css)

--- client/src/app/components/Sidebar/index.tsx
+++ client/src/app/components/Sidebar/index.tsx
@@ -38,4 +38,47 @@ const Sidebar = () => {
 	);
 };
 
+interface SidebarLinkProps {
+	href: string;
+	icon: LucideIcon;
+	label: string;
+	//isCollapsed: boolean;
+}
+
+const SidebarLink = ({
+	href,
+	icon: Icon,
+	label,
+}: //isCollapsed,
+SidebarLinkProps) => {
+	const pathName = usePathname();
+	const isActive =
+		pathName === href || (pathName === '/' && href === '/dashboard');
+	const screenWidth = window.innerWidth;
+
+	const dispatch = useAppDispatch();
+	const isSideBarCollapsed = useAppSelector(
+		(state) => state.global.isSideBarCollapsed
+	);
+
+	return (
+		<Link href={href} className="w-full">
+			<div
+				className={` justify-start px-8 py-3 relative flex cursor-pointer items-center gap-3 transition-colors
+				 hover:bg-gray-100 dark:bg-black dark:hover:bg-gray-700 
+				 ${isActive ? 'bg-gray-100 text-white dark:bg-gray-600' : ''}`}
+			>
+				{isActive && (
+					<div className="absolute left-0 top-0 h-[100%] w-[10px] bg-gray-950 dark:bg-gray-200" />
+				)}
+
+				<Icon className="h-6 w-6 text-gray-800 dark:text-gray-100" />
+				<span className="font-medium text-gray-800 dark:text-gray-100">
+					{label}
+				</span>
+			</div>
+		</Link>
+	);
+};
+
 export default Sidebar;
