9ab8c891d0924252f498429ecbc0d2613c81ab9e
JuniorFs <juniorfernandessilva1@gmail.com>
2025-04-23 22:14:03 -0300

    Update dashboardWrapper.tsx
    Updating wrapper css, adjusting responsiveness

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -22,7 +22,7 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
 	});
 
 	return (
-		<div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
+		<div className="flex min-h-screen w-full min-w-[370px] bg-gray-50 text-gray-900">
 			<Sidebar />
 			<main
 				className={`flex w-full flex-col  bg-gray-50 dark:bg-dark-bg ${
