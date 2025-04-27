a8ca653aa203a0ac88a69ad6fe0a740bf3bc9971
JuniorFs <juniorfernandessilva1@gmail.com>
2025-04-27 04:00:02 -0300

    Update page.tsx
    adjusting the projects use state

--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { use, useState } from 'react';
 import ProjectHeader from '@/app/projects/projectHeader';
+import BoardView from '../BoardView';
 
 type Props = {
 	params: Promise<{ id: string }>;
@@ -9,12 +10,16 @@ type Props = {
 
 const Project = ({ params }: Props) => {
 	const { id } = use(params);
-	const { activeTab, setActiveTab } = useState('Board');
-	const { isModalNewTaskOpen, setModalNewTaskOpen } = useState<boolean>(false);
+	const [activeTab, setActiveTab] = useState('Board');
+	const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
 
 	return (
 		<div>
 			<ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
+
+			{activeTab === 'Board' && (
+				<BoardView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+			)}
 		</div>
 	);
 };
