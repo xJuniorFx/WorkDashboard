d593d3bbd2447ac47a00fb4b0361fd381eafdac9
JuniorFs <juniorfernandessilva1@gmail.com>
2025-05-16 00:56:58 -0300

    Update index.tsx
    Removing Header

--- client/src/app/projects/ListView/index.tsx
+++ client/src/app/projects/ListView/index.tsx
@@ -21,9 +21,6 @@ const ListView = ({ id, setIsModalNewTaskOpen }: ListProps) => {
 
 	return (
 		<div className="p-4 pb-8 xl:p-6">
-			<div className="pt-5">
-				<Header name="List" />
-			</div>
 			<div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 lg:gap-6">
 				{tasks?.map((task: Task) => (
 					<TaskCard key={task.id} task={task} />
