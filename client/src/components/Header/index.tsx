82e07d4969d90ed48be4616c855ca8d4b1f179ad
JuniorFs <juniorfernandessilva1@gmail.com>
2025-05-16 00:57:42 -0300

    Update index.tsx
    Removing header

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -9,10 +9,7 @@ type Props = {
 const Header = ({ name, buttoComponent, isSmallText }: Props) => {
 	return (
 		<div className="mb-5 flex w-full">
-			<div
-				className={'w-2 !bg-[#2563EB] rounded-s-lg'}
-				style={{ backgroundColor: '#2563EB' }}
-			/>
+			<div className={'w-2 rounded-s-lg bg-[#e42974] dark:bg-[#2563EB]'} />
 			<div className="flex w-full items-center justify-between rounded-e-lg bg-white px-5 py-4 dark:bg-dark-secundary ">
 				<h3 className="flex items-center text-lg font-bold dark:text-white">
 					{name}
