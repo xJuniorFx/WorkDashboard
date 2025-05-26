305e0316146a95cb87e88d318978c8e8a9f74f43
JuniorFs <juniorfernandessilva1@gmail.com>
2025-05-25 23:39:33 -0300

    Update page.tsx
    Creating the table view

--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -5,6 +5,7 @@ import ProjectHeader from '@/app/projects/projectHeader';
 import BoardView from '../BoardView';
 import ListView from '../ListView';
 import Timeline from '../TimelineView';
+import TableView from '../TableView';
 
 type Props = {
 	params: Promise<{ id: string }>;
@@ -27,6 +28,9 @@ const Project = ({ params }: Props) => {
 			)}
 			{activeTab === 'Timeline' && (
 				<Timeline id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+			)}
+			{activeTab === 'Table' && (
+				<TableView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
 			)}
 		</div>
 	);
