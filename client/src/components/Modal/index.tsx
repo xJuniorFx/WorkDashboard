7e9c7eb97cfe8754ae111742fcf499317522a373
JuniorFs <juniorfernandessilva1@gmail.com>
2025-06-07 19:29:42 -0300

    Creating Modal

---
+++ client/src/components/Modal/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from '../Header';
+import { X } from 'lucide-react';
+
+type Props = {
+	children: React.ReactNode;
+	isOpen: boolean;
+	onClose: () => void;
+	name: string;
+};
+
+const Modal = ({ children, isOpen, onClose, name }: Props) => {
+	if (!isOpen) return null;
+
+	return ReactDOM.createPortal(
+		<div className="fixed inset-0 z-50 flex w-full h-full items-center justify-center overflow-y-auto bg-gray-700 bg-opacity-70 p-4">
+			<div className="w-full max-w-2xl rounded-lg bg-white p-4 shadow-lg dark:bg-dark-secundary">
+				<Header
+					name={name}
+					isSmallText
+					buttoComponent={
+						<button
+							className="flex h-7 w-7 items-center justify-center rounded bg-[#e42974] px-3 py-2 text-white hover:bg-[#801741] dark:bg-[#2563EB] dark:hover:bg-[#14357d]"
+							onClick={onClose}
+						>
+							<X size={18} />
+						</button>
+					}
+				/>
+				{children}
+			</div>
+		</div>,
+		document.body
+	);
+};
+
+export default Modal;
