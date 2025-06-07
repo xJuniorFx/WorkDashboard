fe6fad1be1cdd366b99ae169dfbfb407685f5899
JuniorFs <juniorfernandessilva1@gmail.com>
2025-06-07 19:31:01 -0300

    Update projectHeader.tsx
    Adjusting the project Header and its Styling and adding the newProject button

--- client/src/app/projects/projectHeader.tsx
+++ client/src/app/projects/projectHeader.tsx
@@ -1,7 +1,16 @@
 import Header from '@/components/Header';
 import TabButton from '@/app/projects/BoardView/tabButton';
-import { Clock, Filter, Grid3x3, List, Share2, Table } from 'lucide-react';
+import {
+	Clock,
+	Filter,
+	Grid3x3,
+	List,
+	PlusSquare,
+	Share2,
+	Table,
+} from 'lucide-react';
 import React, { useState } from 'react';
+import ModalNewProject from './ModalNewProject';
 
 type Props = {
 	activeTab: string;
@@ -9,13 +18,27 @@ type Props = {
 };
 
 const ProjectHeader = ({ activeTab, setActiveTab }: Props) => {
-	const { isModalNewProjectOpen, setIstModalNewProjectOpen } =
-		useState<boolean>(false);
+	const { isModalNewProjectOpen, setIstModalNewProjectOpen } = useState(false);
 
 	return (
 		<div className="pl-8 pr-4 xl:pl-10">
+			<ModalNewProject
+				isOpen={isModalNewProjectOpen}
+				onClose={() => setIstModalNewProjectOpen(false)}
+			/>
 			<div className="pb-6 pt-6 lg:pb-4 lg:pt-8">
-				<Header name={'Product Design Development'} />
+				<Header
+					name={'Product Design Development'}
+					buttoComponent={
+						<button
+							className="flex items-center justify-center rounded-md bg-[#e42974] px-3 py-2 text-white hover:bg-[#801741] dark:bg-[#2563EB] dark:hover:bg-[#14357d]"
+							onClick={() => setIstModalNewProjectOpen(true)}
+						>
+							<PlusSquare className="mr-2 h-5 w-5" />
+							New Board
+						</button>
+					}
+				/>
 			</div>
 			<div className="flex flex-col gap-2 border-y border-gray-200 pb-[8px] pt-2 dark:border-stroke-dark md:flex-row md:items-center md:justify-between">
 				<div className="flex flex-wrap items-center sm:gap-6 gap-4">
