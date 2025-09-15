d204f6b72e1d1cd50b1a3c03bb6bcf6c72e0c8cf
JuniorFs <juniorfernandessilva1@gmail.com>
2025-09-15 18:33:12 -0300

    Add AWS Cognito authentication integration
    Introduces AWS Cognito environment variables and adds dependencies for aws-amplify and @aws-amplify/ui-react. Implements a new authProvider component and updates dashboardWrapper to support authentication.

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -4,6 +4,7 @@ import React, { useEffect } from 'react';
 import Sidebar from '@/components/Sidebar/sidebar';
 import Navbar from '@/components/Navbar';
 import StoreProvider, { useAppSelector } from './redux';
+import AuthProvider from './authProvider';
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
 	const isSidebarCollapsed = useAppSelector(
@@ -39,7 +40,9 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<StoreProvider>
-			<DashboardLayout>{children}</DashboardLayout>
+			<AuthProvider>
+				<DashboardLayout>{children}</DashboardLayout>
+			</AuthProvider>
 		</StoreProvider>
 	);
 };
